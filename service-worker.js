726297aa78e06b4d5b25e38389a0624676e493d3
Masaki Mizuno <masaki.mizuno@testfirst.dev>
2024-06-20 09:24:04 +0900

    pwa

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'my-pwa-cache-v1';
+const urlsToCache = [
+    '/',
+    '/styles.css',
+    '/app.js',
+    '/index.html'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
